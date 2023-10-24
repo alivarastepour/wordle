5c485269124c0078eb34493bc76a50fc407a1773
alivarastepour <alivaraste@outlook.com>
2023-10-24 10:16:03 +0330

    added a portion of game logic

--- src/components/GameWrapper/GameWrapper.tsx
+++ src/components/GameWrapper/GameWrapper.tsx
@@ -1,35 +1,28 @@
 import GameTable from "../GameTable/GameTable";
 import Keyboard from "../Keyboard/Keyboard";
 import { useState } from "react";
-import { IGuessItem } from "../../interfaces/IGuessItem";
 import { TGuesses } from "../../types/TGuesses";
 import { MAX_GUESS_COUNT } from "../../lib/constants";
-// keyboard does not need word data. but it should be able to set it.
-//it needs data about placement
 
 const guessesInitialState: TGuesses = new Array(MAX_GUESS_COUNT).fill({
   word: "",
   accuracy: [],
 });
 
-// gameboard needs words but does not set them. it produces data aobut placement
 function GameWrapper() {
-  const [guesses, setGuesses] = useState<TGuesses>([
-    {
-      accuracy: [0, 1, 0, 2, 1],
-      word: "salam",
-    },
-    {
-      accuracy: [0, 0, 1, 1, 2],
-      word: "alooa",
-    },
-  ]);
-  const [currentGuess, setCurrentGuess] = useState<string>("abcde");
+  const [guesses, setGuesses] = useState<TGuesses>(guessesInitialState);
+  const [currentGuess, setCurrentGuess] = useState<string>("");
+  // console.log(guesses);
+  // console.log(currentGuess);
 
   return (
     <>
       <GameTable guesses={guesses} currentGuess={currentGuess} />
-      <Keyboard />
+      <Keyboard
+        currentGuess={currentGuess}
+        setGuesses={setGuesses}
+        setCurrentGuess={setCurrentGuess}
+      />
     </>
   );
 }
--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,5 +1,32 @@
 import { useMemo, useEffect } from "react";
 import styles from "../../styles/keyboard.module.scss";
+import { TSetState } from "../../types/TSetState";
+import { MAX_GUESS_LENGTH, WORD } from "../../lib/constants";
+import { IGuessItem } from "../../interfaces/IGuessItem";
+import { TGuesses } from "../../types/TGuesses";
+
+function determineAccuracy(word: string) {
+  const res = [];
+  for (let index = 0; index < word.length; index++) {
+    const current = word[index];
+
+    const rightPlace = current === WORD[index];
+    if (rightPlace) {
+      res.push(2);
+      continue;
+    }
+
+    const wrongPlace = WORD.includes(current);
+    if (wrongPlace) {
+      res.push(1);
+      continue;
+    }
+
+    res.push(0);
+  }
+
+  return res;
+}
 
 function getKeyboardChars() {
   //consider qwerty style
@@ -9,7 +36,15 @@ function getKeyboardChars() {
     .concat(...["backspace", "enter"]);
 }
 
-function Keyboard() {
+function Keyboard({
+  currentGuess,
+  setCurrentGuess,
+  setGuesses,
+}: {
+  currentGuess: string;
+  setGuesses: TSetState<TGuesses>;
+  setCurrentGuess: TSetState<string>;
+}) {
   const letters = useMemo(getKeyboardChars, []);
 
   useEffect(() => {
@@ -21,7 +56,37 @@ function Keyboard() {
       const container = e.currentTarget;
       if (Object.is(target, container)) return;
 
-      console.log((target as Element).innerHTML);
+      const element = target as HTMLElement;
+      const action = element.dataset.action;
+      const content = element.innerText;
+
+      switch (action) {
+        case "letter":
+          if (currentGuess.length === MAX_GUESS_LENGTH) break;
+
+          setCurrentGuess((prev) => prev.concat(content));
+          break;
+        case "del":
+          setCurrentGuess((prev) => {
+            const length = prev.length;
+            if (length === 0) return prev;
+
+            const newCurrentGuess = prev.slice(0, length - 1);
+            return newCurrentGuess;
+          });
+          break;
+        case "submit":
+          const accuracy = determineAccuracy(currentGuess);
+          const newGuess: IGuessItem = {
+            accuracy,
+            word: currentGuess,
+          };
+          console.log(accuracy);
+
+          setGuesses((prev) => prev.concat(newGuess));
+          setCurrentGuess("");
+          break;
+      }
     }
 
     keyboardWrapper.addEventListener("click", handleKeyboardClick);
@@ -30,14 +95,21 @@ function Keyboard() {
       if (!keyboardWrapper) return;
       keyboardWrapper.removeEventListener("click", handleKeyboardClick);
     };
-  }, []);
+  }, [currentGuess]);
 
   return (
     <>
       <div id="keyboard-wrapper" className={styles["keyboard-wrapper"]}>
         {letters.map((letter) => {
+          const action =
+            letter === "backspace"
+              ? "del"
+              : letter === "enter"
+              ? "submit"
+              : "letter";
           return (
             <div
+              data-action={action}
               className={`${styles[letter]} ${styles["keyboard-letter"]}`}
               key={letter}
             >
