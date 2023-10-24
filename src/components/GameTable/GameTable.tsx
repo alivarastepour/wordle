4e4a14de12f696a31c360999bc1bb1d95d574aaa
alivarastepour <alivaraste@outlook.com>
2023-10-24 09:35:12 +0330

    completed view layer

--- src/components/GameTable/GameTable.tsx
+++ src/components/GameTable/GameTable.tsx
@@ -1,5 +1,43 @@
-function GameTable() {
-  return <>gametable</>;
+import { MAX_GUESS_COUNT } from "../../lib/constants";
+import styles from "../../styles/gameTable.module.scss";
+import { TGuesses } from "../../types/TGuesses";
+import EmptyRow from "./EmptyRow";
+import FilledRow from "./FilledRow";
+import FillingRow from "./FillingRow";
+import { useMemo } from "react";
+function GameTable({
+  guesses,
+  currentGuess,
+}: {
+  guesses: TGuesses;
+  currentGuess: string;
+}) {
+  const filledRows = useMemo(
+    () => guesses.filter(({ word }) => !!word),
+    [guesses]
+  );
+  const emptyRows = new Array(MAX_GUESS_COUNT - filledRows.length).fill(null);
+  return (
+    <>
+      <div className={styles["game-table-wrapper"]}>
+        <div className={`${"row-flex"} ${styles["filled-row-wrapper"]}`}>
+          {guesses
+            .filter(({ word }) => !!word)
+            .map((item, index) => {
+              return <FilledRow key={`${item.word}-${index}`} info={item} />;
+            })}
+        </div>
+        <div className={`${"row-flex"} ${styles["filling-row-wrapper"]}`}>
+          <FillingRow word={currentGuess} />
+        </div>
+        <div className={`${"row-flex"} ${styles["empty-row-wrapper"]}`}>
+          {emptyRows.map((_, index) => {
+            return <EmptyRow key={index} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default GameTable;
--- src/components/GameWrapper/GameWrapper.tsx
+++ src/components/GameWrapper/GameWrapper.tsx
@@ -1,10 +1,34 @@
 import GameTable from "../GameTable/GameTable";
 import Keyboard from "../Keyboard/Keyboard";
+import { useState } from "react";
+import { IGuessItem } from "../../interfaces/IGuessItem";
+import { TGuesses } from "../../types/TGuesses";
+import { MAX_GUESS_COUNT } from "../../lib/constants";
+// keyboard does not need word data. but it should be able to set it.
+//it needs data about placement
 
+const guessesInitialState: TGuesses = new Array(MAX_GUESS_COUNT).fill({
+  word: "",
+  accuracy: [],
+});
+
+// gameboard needs words but does not set them. it produces data aobut placement
 function GameWrapper() {
+  const [guesses, setGuesses] = useState<TGuesses>([
+    {
+      accuracy: [0, 1, 0, 2, 1],
+      word: "salam",
+    },
+    {
+      accuracy: [0, 0, 1, 1, 2],
+      word: "alooa",
+    },
+  ]);
+  const [currentGuess, setCurrentGuess] = useState<string>("abcde");
+
   return (
     <>
-      <GameTable />
+      <GameTable guesses={guesses} currentGuess={currentGuess} />
       <Keyboard />
     </>
   );
