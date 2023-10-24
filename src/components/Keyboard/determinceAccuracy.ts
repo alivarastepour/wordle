df7a12322bc96fbf02d483ca1b7b166c528811bf
alivarastepour <alivaraste@outlook.com>
2023-10-24 11:23:29 +0330

    dummy

--- src/components/Keyboard/determinceAccuracy.ts
+++ src/components/Keyboard/determinceAccuracy.ts
@@ -1,7 +1,20 @@
 import { WORD } from "../../lib/constants";
 
+// function getDic() {
+//   const map: { [index: string]: number } = {};
+//   for (const letter of WORD) {
+//     const val = map[letter];
+//     map[letter] = val ? val + 1 : 1;
+//   }
+//   return map;
+// }
+
 export function determineAccuracy(word: string) {
+  // consider: should work with repeating letters
   const res = [];
+  // const a = getDic();
+  // console.log(a);
+
   for (let index = 0; index < word.length; index++) {
     const current = word[index];
 
