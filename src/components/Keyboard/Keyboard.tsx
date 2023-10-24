5f31087f7506062b98f29b034f8235a99193838c
alivarastepour <alivaraste@outlook.com>
2023-10-24 08:30:58 +0330

    added more content for the Keyboard component

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,5 +1,53 @@
+import { useMemo, useEffect } from "react";
+import styles from "../../styles/keyboard.module.scss";
+
+function getKeyboardChars() {
+  //consider qwerty style
+  return [...Array(26).keys()]
+    .map((i) => i + 65)
+    .map((i) => String.fromCharCode(i))
+    .concat(...["backspace", "enter"]);
+}
+
 function Keyboard() {
-  return <>keyboard</>;
+  const letters = useMemo(getKeyboardChars, []);
+
+  useEffect(() => {
+    const keyboardWrapper = document.getElementById("keyboard-wrapper");
+    if (!keyboardWrapper) return;
+
+    function handleKeyboardClick(e: MouseEvent) {
+      const target = e.target;
+      const container = e.currentTarget;
+      if (Object.is(target, container)) return;
+
+      console.log((target as Element).innerHTML);
+    }
+
+    keyboardWrapper.addEventListener("click", handleKeyboardClick);
+
+    return () => {
+      if (!keyboardWrapper) return;
+      keyboardWrapper.removeEventListener("click", handleKeyboardClick);
+    };
+  }, []);
+
+  return (
+    <>
+      <div id="keyboard-wrapper" className={styles["keyboard-wrapper"]}>
+        {letters.map((letter) => {
+          return (
+            <div
+              className={`${styles[letter]} ${styles["keyboard-letter"]}`}
+              key={letter}
+            >
+              {letter}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Keyboard;
